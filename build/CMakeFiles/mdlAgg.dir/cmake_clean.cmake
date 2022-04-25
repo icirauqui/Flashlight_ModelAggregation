ff58de018f0767aca9fcc0c776220aff739c6828
icirauqui <icirauqui@gmail.com>
2022-04-25 16:42:02 +0000

    init

---
+++ build/CMakeFiles/mdlAgg.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mdlAgg.dir/src/main.cpp.o"
+  "mdlAgg"
+  "mdlAgg.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mdlAgg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+
+
+int main() {
+    std::cout << "Flashlight_ModelAggregation" << std::endl;
+
+    return 0;
+}
